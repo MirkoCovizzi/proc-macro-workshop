04fc7e01c89babd806ef72704c31b9f1bf1d9305
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-18 15:34:58 +0200

    Builder: pass tests/01-parse

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,10 +1,12 @@
 extern crate proc_macro;
+extern crate syn;
 
 use proc_macro::TokenStream;
+use syn::{parse_macro_input, DeriveInput};
+use syn::export::ToTokens;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let _ = input;
-
-    unimplemented!()
+    let input = parse_macro_input!(input as DeriveInput);
+    TokenStream::new()
 }
