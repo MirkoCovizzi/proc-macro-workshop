637b6fed9d013a5af4f280d93aec7e4ec4a24624
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-18 16:29:14 +0200

    Builder: pass tests/03-call-setters

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -25,6 +25,28 @@ pub fn derive(input: TokenStream) -> TokenStream {
             current_dir: Option<String>,
         }
 
+        impl #builder_ident {
+            fn executable(&mut self, executable: String) -> &mut Self {
+                self.executable = Some(executable);
+                self
+            }
+
+            fn args(&mut self, args: Vec<String>) -> &mut Self {
+                self.args = Some(args);
+                self
+            }
+
+            fn env(&mut self, env: Vec<String>) -> &mut Self {
+                self.env = Some(env);
+                self
+            }
+
+            fn current_dir(&mut self, current_dir: String) -> &mut Self {
+                self.current_dir = Some(current_dir);
+                self
+            }
+        }
+
         impl #name {
             pub fn builder() -> #builder_ident {
                 #builder_ident {
