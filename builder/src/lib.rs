57beeb4e9f1542cb0d6a5d206ef0bccd43fe9694
Mirko Covizzi <mrkcvzz@gmail.com>
2020-04-18 16:18:08 +0200

    Builder: pass tests/02-create-builder

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,12 +1,40 @@
 extern crate proc_macro;
 extern crate syn;
+extern crate quote;
 
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 use syn::export::ToTokens;
+use quote::quote;
+use std::process::exit;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
-    let input = parse_macro_input!(input as DeriveInput);
-    TokenStream::new()
+    let syntax_tree = parse_macro_input!(input as DeriveInput);
+    println!("{:#?}", syntax_tree);
+
+    let name = &syntax_tree.ident;
+    let builder_name = format!("{}Builder", name);
+    let builder_ident = syn::Ident::new(&builder_name, name.span());
+
+    let extended = quote! {
+        pub struct #builder_ident {
+            executable: Option<String>,
+            args: Option<Vec<String>>,
+            env: Option<Vec<String>>,
+            current_dir: Option<String>,
+        }
+
+        impl #name {
+            pub fn builder() -> #builder_ident {
+                #builder_ident {
+                    executable: None,
+                    args: None,
+                    env: None,
+                    current_dir: None,
+                }
+            }
+        }
+    };
+    extended.into()
 }
